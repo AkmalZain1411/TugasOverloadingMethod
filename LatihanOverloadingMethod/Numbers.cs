d3aacf9369be4606da4e08fa635ebe68c11b6c95
AkmalZain <muhammad8071@students.amikom.ac.id>
2021-04-01 22:48:39 +0700

    tugas Overloading Method

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            int min = number2;
+            return min;
+            
+        }
+        public int FindMinimum(int number1, int number2, float number3 )
+        {
+            int min = number2;
+            return min;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            int max = number1;
+            return max;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            int max = (int) number3;
+            return max;
+        }
+    }
+}
